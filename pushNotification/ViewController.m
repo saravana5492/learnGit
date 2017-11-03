44bfeec00b5a1406e468578a8b5e1a0050a5c080
saravana5492 <32759282+saravana5492@users.noreply.github.com>
2017-11-03 20:06:43 +0530

    Sarwan

--- pushNotification/ViewController.m
+++ pushNotification/ViewController.m
@@ -33,6 +33,10 @@
     
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
