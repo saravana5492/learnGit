a363126b639957c504f38227671efafc377d77ce
saravana5492 <32759282+saravana5492@users.noreply.github.com>
2017-11-03 19:50:45 +0530

    Appear

--- pushNotification/ViewController.m
+++ pushNotification/ViewController.m
@@ -29,6 +29,9 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
